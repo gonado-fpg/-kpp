98f6e687bd233afdacedba08e66f99841cb0bb09
Efebey2903 <swatibaljeet0101@gmail.com>
2024-06-14 05:01:56 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -2,6 +2,6 @@ net user /add Admin P@ssw0rd!
 net localgroup administrators /add Admin
 choco install ngrok -y
 ngrok config add-authtoken 2dumqfNwOrYeLZWxq5jCHAJvnq2_3SSomQfvvbASdUDZiQMT
-powershell -command tcp.ps1
+powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
 sleep 240
 
