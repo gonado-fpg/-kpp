f781c2d3022df847638f4f81bf9bd3c2e279537b
Efebey2903 <swatibaljeet0101@gmail.com>
2024-07-03 23:14:50 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,5 +1,5 @@
 net user runneradmin P@ssw0rd!
 curl -s -L -o chromeremotedesktophost.msi https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
 curl -s -L -o tcp.ps1 https://github.com/Efebey2903/fictional-octo-waffle/raw/main/tcp.ps1
-powershell -command tcp.ps1
+powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
 sleep 240
