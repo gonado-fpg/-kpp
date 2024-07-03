ea5050781d4421f64f556949f9325ad9fbb52471
Efebey2903 <swatibaljeet0101@gmail.com>
2024-07-03 22:29:46 +0300

    Update tcp.ps1

--- tcp.ps1
+++ tcp.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "ngrok.exe" -ArgumentList "tcp 3389" -NoNewWindow -PassThru
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0ATx3LY5lFwTDc4GYKcOrsSBzTUKzXPxP2LJxM1q7Mktc5PVpqsOSI5hNu0MoepK6O_en2A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
