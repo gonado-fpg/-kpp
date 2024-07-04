8431bddd27335dd2f814f7b0b5992eb9333f7735
Efebey2903 <swatibaljeet0101@gmail.com>
2024-07-04 18:30:58 +0300

    Update tcp.ps1

--- tcp.ps1
+++ tcp.ps1
@@ -1,1 +1,1 @@
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0ATx3LY6Z5tG6gJte9QNnciT7r05YgP6dHQLa53w6pOrTU9wMN9A8iOsfNrzfNOtQcKHD8A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0ATx3LY5SvtjqhB9x9zXSc6MGqsUnqJ5gJ_yAUjpamy1FADuQ9CmNyt9_0GjNliNDgUFypg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
