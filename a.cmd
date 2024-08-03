eaf40ddcdea53e8508ce319b59e7a397dbd158c5
gonado-fpg <gonad-remold-claw@duck.com>
2024-08-03 05:41:21 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,6 +1,6 @@
 net user runneradmin P@ssw0rd!
 curl -s -L -o chromeremotedesktophost.msi https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
-curl -s -L -o tcp.ps1 https://github.com/Efebey2903/fictional-octo-waffle/raw/main/tcp.ps1
+curl -s -L -o tcp.ps1 https://github.com/gonado-fpg/fictional-octo-waffle/raw/main/tcp.ps1
 choco install ngrok -y
 ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
 powershell -command D:\a\fictional-octo-waffle\fictional-octo-waffle\tcp.ps1
